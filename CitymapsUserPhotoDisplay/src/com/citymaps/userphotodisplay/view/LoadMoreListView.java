050520ea670ebde2df38f8aa5147837a8045e566
drice0331 <d.rice0331@gmail.com>
2015-02-18 16:38:10 -0800

    committing and pushing to repo

---
+++ CitymapsUserPhotoDisplay/src/com/citymaps/userphotodisplay/view/LoadMoreListView.java
@@ -0,0 +1,161 @@
+package com.citymaps.userphotodisplay.view;
+
+
+import com.citymaps.userphotodisplay.R;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+/**
+ * Custom listview that loads more items when scrolling to the bottom
+ * @author drice
+ *
+ */
+public class LoadMoreListView extends ListView implements OnScrollListener {
+
+	private static final String TAG = "LoadMoreListView";
+
+
+	private OnScrollListener mOnScrollListener;
+	private LayoutInflater mInflater;
+
+	private RelativeLayout mFooterView;
+	private ProgressBar mProgressBarLoadMore;
+
+	private OnLoadMoreListener mOnLoadMoreListener;
+
+	private boolean mIsLoadingMore = false;
+	private int mCurrentScrollState;
+
+	public LoadMoreListView(Context context) {
+		super(context);
+		init(context);
+	}
+
+	public LoadMoreListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(context);
+	}
+
+	public LoadMoreListView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init(context);
+	}
+
+	private void init(Context context) {
+
+		mInflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		// footer
+		mFooterView = (RelativeLayout) mInflater.inflate(
+				R.layout.load_more_footer, this, false);
+
+		mProgressBarLoadMore = (ProgressBar) mFooterView
+				.findViewById(R.id.load_more_progressBar);
+
+		addFooterView(mFooterView);
+
+		super.setOnScrollListener(this);
+	}
+
+	@Override
+	public void setAdapter(ListAdapter adapter) {
+		super.setAdapter(adapter);
+	}
+
+	/**
+	 * Set the listener that will receive notifications every time the list
+	 * scrolls.
+	 * 
+	 * @param scroll listener.
+	 */
+	@Override
+	public void setOnScrollListener(AbsListView.OnScrollListener l) {
+		mOnScrollListener = l;
+	}
+
+	/**
+	 * Register a callback to be invoked when this list reaches the end (last
+	 * item be visible)
+	 * 
+	 * @param onLoadMoreListener
+	 */
+	public void setOnLoadMoreListener(OnLoadMoreListener onLoadMoreListener) {
+		mOnLoadMoreListener = onLoadMoreListener;
+	}
+
+	public void onScroll(AbsListView view, int firstVisibleItem,
+			int visibleItemCount, int totalItemCount) {
+
+		if (mOnScrollListener != null) {
+			mOnScrollListener.onScroll(view, firstVisibleItem,
+					visibleItemCount, totalItemCount);
+		}
+
+		if (mOnLoadMoreListener != null) {
+
+			if (visibleItemCount == totalItemCount) {
+				mProgressBarLoadMore.setVisibility(View.GONE);
+				return;
+			}
+
+			boolean loadMore = firstVisibleItem + visibleItemCount >= totalItemCount;
+
+			if (!mIsLoadingMore && loadMore
+					&& mCurrentScrollState != SCROLL_STATE_IDLE) {
+				mProgressBarLoadMore.setVisibility(View.VISIBLE);
+				mIsLoadingMore = true;
+				onLoadMore();
+			}
+
+		}
+
+	}
+
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+	
+		if ( scrollState == OnScrollListener.SCROLL_STATE_IDLE )
+        	{
+          		view.invalidateViews();
+        	}
+        	
+		mCurrentScrollState = scrollState;
+
+		if (mOnScrollListener != null) {
+			mOnScrollListener.onScrollStateChanged(view, scrollState);
+		}
+
+	}
+
+	public void onLoadMore() {
+		Log.d(TAG, "onLoadMore");
+		if (mOnLoadMoreListener != null) {
+			mOnLoadMoreListener.onLoadMore();
+		}
+	}
+
+
+	public void onLoadMoreComplete() {
+		mIsLoadingMore = false;
+		mProgressBarLoadMore.setVisibility(View.GONE);
+	}
+
+	public interface OnLoadMoreListener {
+		/**
+		 * Called when the list reaches the last item (the last item is visible
+		 * to the user)
+		 */
+		public void onLoadMore();
+	}
+
+}
